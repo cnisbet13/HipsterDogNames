f8e3ba9ea914eeccc736ab8a95fc9c04ebf1f5e7
Calvin Nisbet <calvinnisbet@Calvins-MacBook-Air.local>
2015-10-04 14:21:36 -0700

    Finished, except App Icon

--- HipsterDogNames/ViewController.swift
+++ HipsterDogNames/ViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
+
+
+
+
+
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var genderSegment: UISegmentedControl!
     @IBOutlet weak var nameGeneratorButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
-    var maleDogNames: NSArray!
+  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
          self.navigationController?.navigationBar.topItem?.title = "Hipster Dog Names"
+ 
         
-         maleDogNames = ["Abe", "Ace", "Alden", "Alfie", "Angus", "Archer", "Angus",
-            "Archer", "Arlo", "Arnold", "Arthur", "Asher", "Atticus", "Avalon", "Avis", "Bandit", "Baron",
-            "Bess", "Boaz", "Bruno", "Bryce", "Butch", "Byron", "Callie", "Calvin", "Carlton", "Casper", "Chandler", "Chester", "Clive", "Colt", "Coye", "Dante", "Davis", "Delcan","Desmond", "Dexter", "Duke", "Duncan", "Dusty", "Earl", "Edgar", "Everett","Felix","Finn", "Frankie", "Griffin", "Gus", "Harlow", "Harold", "Holden", "Hopper", "Howie", "Hugo", "Ike", "Jasper", "Judd", "Kai", "Kale", "Kipp", "Knox", "Leon", "Leonard", "Levi", "Marco", "Mervin", "Miles", "Milo", "Milton", "Monty", "Nico", "Oliver", "Orson", "Oscar", "Otis", "Percy", "Ricky", "Rowan", "Rufus", "Ryder", "Sampson", "Sanford", "Scout", "Sebastien", "Silas", "Sullivan", "Waldo", "Wilbur", "Winslow", "Zander"]
-            
 
-        print(maleDogNames)
-        
-        
-        let femaleDogNames = ["Alice", "Apple", "Avis", "Beatrix", "Bess", "Bonnie", "Brooklyn", "Clementine", "Clover", "Daisy", "Delilah", "Edie", "Effie", "Ella", "Fifi", "Flora", "Frances", "Georgia", "Hazel", "Ira", "Isla", "Lana", "Lola", "Maisie", "Matilda", "Noelle", "Olive", "Peaches", "Penelope", "Piper", "Sadie", "Scarlet", "Stella", "Sylvie", "Tillie", "Viola", "Violet", "Willow", "Zola"]
-        print(femaleDogNames)
         
         
     }
@@ -40,13 +39,34 @@ class ViewController: UIViewController {
     }
 
 
+    func nameRandom() {
+        
+        
+    }
+    
  
     @IBAction func nameGenerator(sender: AnyObject) {
         
+        let  maleDogNames = ["Abe", "Ace", "Alden", "Alfie", "Angus", "Archer", "Angus",
+            "Archer", "Arlo", "Arnold", "Arthur", "Asher", "Atticus", "Avalon", "Avis", "Bandit", "Baron",
+            "Bess", "Boaz", "Bruno", "Bryce", "Butch", "Byron", "Callie", "Calvin", "Carlton", "Casper", "Chandler", "Chester", "Clive", "Colt", "Coye", "Dante", "Davis", "Delcan","Desmond", "Dexter", "Duke", "Duncan", "Dusty", "Earl", "Edgar", "Everett","Felix","Finn", "Frankie", "Griffin", "Gus", "Harlow", "Harold", "Holden", "Hopper", "Howie", "Hugo", "Ike", "Jasper", "Judd", "Kai", "Kale", "Kipp", "Knox", "Leon", "Leonard", "Levi", "Marco", "Mervin", "Miles", "Milo", "Milton", "Monty", "Nico", "Oliver", "Orson", "Oscar", "Otis", "Percy", "Ricky", "Rowan", "Rufus", "Ryder", "Sampson", "Sanford", "Scout", "Sebastien", "Silas", "Sullivan", "Waldo", "Wilbur", "Winslow", "Zander"]
+        
+        let maleIndex = Int(arc4random_uniform(UInt32(maleDogNames.count)))
+        
+        
+        // let randomMaleName = maleDogNames[Int(arc
+        //  print(maleDogNames)
+        
+        
+        let femaleDogNames = ["Alice", "Apple", "Avis", "Beatrix", "Bess", "Bonnie", "Brooklyn", "Clementine", "Clover", "Daisy", "Delilah", "Edie", "Effie", "Ella", "Fifi", "Flora", "Frances", "Georgia", "Hazel", "Ira", "Isla", "Lana", "Lola", "Maisie", "Matilda", "Noelle", "Olive", "Peaches", "Penelope", "Piper", "Sadie", "Scarlet", "Stella", "Sylvie", "Tillie", "Viola", "Violet", "Willow", "Zola"]
+        
+        let femaleIndex = Int(arc4random_uniform(UInt32(femaleDogNames.count)))
+        
+        
         if genderSegment.selectedSegmentIndex == 0 {
-                nameLabel.text = maleDogNames.objectAtIndex(3) as? String
+                nameLabel.text = maleDogNames[maleIndex]
         } else {
-            maleDogNames.objectAtIndex(4) as? String
+           nameLabel.text = femaleDogNames[femaleIndex]
         }
     }
     
